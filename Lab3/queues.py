33ff50645b0e2195f531eef8619e5ee8e51c4c4d
Tiger Sun <sunchester@Tigers-MacBook-Pro.local>
2017-09-29 01:30:47 -0700

    Updated Lab3 with additional code for Class Queue Linked and added more test cases

--- Lab3/queues.py
+++ Lab3/queues.py
@@ -26,8 +26,51 @@ class QueueArray:
 		return temp
 	def num_in_queue(self):
 		return self.size
-		"""returns the number of items in the queue"""
-# class QueueArray:
-# 	None
-# class QueueLinked:
-# 	None
+		
+class QueueLinked:
+	def __init__(self,capacity):
+		self.capacity = capacity
+		self.front = None
+		self.rear = None
+		self.size = 0
+	def is_empty(self):
+		return self.size == 0
+	def is_full(self):
+		return self.size == self.capacity
+	def enqueue(self,item):
+		newNode = Node(item)
+		if self.size == self.capacity:
+			raise IndexError('Queue is full')
+		if self.size == 0:
+			self.front= newNode
+		if self.rear is not None:
+			self.rear=(self.rear).setNext(newNode)		
+		self.rear= newNode
+		self.size+=1
+	
+
+	def dequeue(self):
+		temp = self.front
+		if self.size == 0:
+			raise IndexError('Queue is empty')
+		if self.size == 1:
+			self.front = None
+			self.size-=1
+			return temp.getData()
+		self.front = (self.front).getNext()
+		self.size-=1
+		return temp.getData()
+	def num_in_queue(self):
+		return self.size
+class Node:
+	def __init__(self,initdata):
+		self.data= initdata
+		self.next=None
+	def getData(self):
+		return self.data
+	def getNext(self):
+		return self.next
+	def setData(self,newdata):
+		self.data = newdata
+	def setNext(self,newdata):
+		self.next = newdata
--- Lab3/test_queues.py
+++ Lab3/test_queues.py
@@ -8,8 +8,35 @@ class TestLab3(unittest.TestCase):
 		with self.assertRaises(IndexError): #Checks if raises IndexError when dequeuing a Queue
 			q1.dequeue()
 		q1.enqueue(1)
+		self.assertEqual(q1.dequeue(),1) #Checks dequeue 
+		q1.enqueue(1)
 		self.assertEqual(q1.num_in_queue(),1) #Checks the number of items in Queue
-		self.assertEqual(q1.items_arr,[1,None,None,None]) #Checks if enqueue worked 
+		self.assertEqual(q1.items_arr,[1,1,None,None]) #Checks if enqueue worked 
+		self.assertEqual(q1.is_empty(),False) #Checks is_empty method on a Queue that has called enqueue once
+		self.assertEqual(q1.is_full(),False) #Checks is_full method on a Queue that has called enqueue once
+		q1.enqueue(2)
+		self.assertEqual(q1.rear,3) #Checks the rear
+		self.assertEqual(q1.num_in_queue(),2) #Checks the number of items in Queue
+		q1.enqueue(3)
+		q1.enqueue(4)
+		with self.assertRaises(IndexError): #Checks if raises IndexError when enqueuing a full Queue
+			q1.enqueue(5)
+		self.assertEqual(q1.is_full(),True) #Checks is_full method on a Queue when all slots are occupied by elements
+		self.assertEqual(q1.num_in_queue(),4) #Checks the number of items in Queue
+		q1.dequeue()
+		self.assertEqual(q1.front,2) #Checks front on Queue after dequeue called
+	   	self.assertEqual(q1.num_in_queue(),3) #Checks the number of items in Queue after calling dequeue
+
+	def test_QueueLinked(self):
+		q1 = queues.QueueLinked(4)
+		self.assertEqual(q1.is_empty(),True) #Checks is_empty method on an empty Queue
+		with self.assertRaises(IndexError): #Checks if raises IndexError when dequeuing a Queue
+			q1.dequeue()
+		q1.enqueue(1)
+		q1.dequeue()
+		self.assertEqual(q1.front, None) #Checks if front changed after dequeue
+		q1.enqueue(1)
+		self.assertEqual(q1.num_in_queue(),1) #Checks the number of items in Queue
 		self.assertEqual(q1.is_empty(),False) #Checks is_empty method on a Queue that has called enqueue once
 		self.assertEqual(q1.is_full(),False) #Checks is_full method on a Queue that has called enqueue once
 		q1.enqueue(2)
@@ -20,9 +47,10 @@ class TestLab3(unittest.TestCase):
 			q1.enqueue(5)
 		self.assertEqual(q1.is_full(),True) #Checks is_full method on a Queue when all slots are occupied by elements
 		self.assertEqual(q1.num_in_queue(),4) #Checks the number of items in Queue
+		self.assertEqual(q1.dequeue(),1) #Checks dequeue 
+		self.assertEqual(q1.num_in_queue(),3) #Checks the number of items in Queue after calling dequeue
+		self.assertEqual(q1.front.getData(), 2) #Checks if front changed after dequeue
 
-	def test_QueueLinked(self):
-		None
 
 if __name__=="__main__":
 	unittest.main()
