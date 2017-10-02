e5221ea591749eb1e0b5027e4bc96d259ba8639f
Tiger Sun <sunchester@Tigers-MacBook-Pro.local>
2017-10-02 14:13:44 -0700

    Added postfix_valid function

--- Assignment2/exp_eval.py
+++ Assignment2/exp_eval.py
@@ -1,4 +1,37 @@
-def infix_to_postfix(infix_expr):
-	None
-def postfix_eval(postfix_expr):
-	None
+from Stacks import StackArray
+
+# def infix_to_postfix(infixexpr):
+#     """Converts an infix expression to an equivalent postfix expression """
+
+#     """Signature:  a string containing an infix expression where tokens are space separated is
+#        the single input parameter and returns a string containing a postfix expression
+#        where tokens are space separated"""
+    
+#     ????? = StackArray(30)
+# ...
+#     tokenList = infixexpr.split()
+# ...
+#     return " ".join(postfixList)
+
+
+# def postfix_eval(postfixExpr):
+#     """  Purpose """
+
+# def doMath(op, op1, op2):
+#     """  Purpose """
+
+def postfix_valid(postfixexpr):
+    """ Tests to see that an input is a solvable postfix expressiojn"""
+    counter = 0
+    if postfixexpr == "":
+        return False
+    tokenList= postfixexpr.split()
+    if tokenList[0] in ('*','-','/','+'):
+        return False
+    for x in tokenList:
+            if x not in ('*','-','/','+'):
+                counter+=1
+            else:
+                counter-=1
+    return counter == 1
+                           
--- Assignment2/exp_eval_testcases.py
+++ Assignment2/exp_eval_testcases.py
@@ -1,9 +1,41 @@
-import exp_eval
 import unittest
+from exp_eval import * 
 
-class TestLab3(unittest.TestCase):
-	None
+class test_expressions(unittest.TestCase):
+    def test_invalid(self):
+        self.assertFalse(postfix_valid(""))
+        self.assertFalse(postfix_valid("2 3"))
+        self.assertFalse(postfix_valid("+ 2 3"))
+        self.assertFalse(postfix_valid("2 - 3 +"))
+ 
+    def test_valid(self):
+        self.assertTrue(postfix_valid("2 3 +"))
+        self.assertTrue(postfix_valid("2 3 -"))
+        self.assertTrue(postfix_valid("2 3 *"))
+        self.assertTrue(postfix_valid("2 3 /"))
+        self.assertTrue(postfix_valid("2 3 + 4 -"))
+        self.assertTrue(postfix_valid("1.0 2.0 -"))
+        self.assertTrue(postfix_valid("4 5 + 3 * 5 6 - 4 5 + * -"))
 
+    # def test_postfixeval1(self):
+    #     self.assertAlmostEqual(postfix_eval("3 5 +"), 8)
 
-if __name__== "__main__":
-	unittest.main()
+    # def test_inToPostBasicAssoc(self):
+    #     self.assertEqual(infix_to_postfix("6 - 3"), "6 3 -")
+
+    # def test_inToPostBasicAssoc1(self):
+    #     self.assertEqual(infix_to_postfix("6 - 3 + 2"), "6 3 - 2 +")
+
+    # def test_inToPostBasicAssoc2(self):
+    #     self.assertEqual(infix_to_postfix("6 ^ 3 ^ 2"), "6 3 2 ^ ^")
+
+    # def test_inToPostBasicAssoc3(self):
+    #     self.assertEqual(infix_to_postfix("6 * ( 3 + 2 )"), "6 3 2 + *")
+
+    # def test_inToPostBasicAssoc5(self):
+    #     self.assertEqual(infix_to_postfix("6"), "6")
+    #     with self.assertRaises(ValueError): 
+    #         postfix_eval("3 0 /")
+
+if __name__ == "__main__":
+    unittest.main()
