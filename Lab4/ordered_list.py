6dbbc3f144b731d9eb8591f147f23d7eb344cc37
Tiger Sun <sunchester@Tigers-MacBook-Pro.local>
2017-10-06 14:45:14 -0700

    Finalized Lab4

--- Lab4/ordered_list.py
+++ Lab4/ordered_list.py
@@ -1,141 +1,189 @@
 class OrderedList():
 
-    def __init__(self):
-        self.head = None
-        self.capacity = 0
-        self.tail = None
+	def __init__(self):
+		self.head = None
+		self.capacity = 0
+		self.tail = None
 
 
-    def add(self,item):
-        current= self.head
-        previous = None
-        stop = False
-        while current != None and not stop:
-            if current.getData() > item: 
-                stop = True
-            else:
-                previous = current
-                current = current.getNext()
-        temp = Node(item)
-        if self.capacity == 0:
-            temp.setNext(None)
-            temp.setPrev(None)
-            self.head = temp
-            self.tail = temp
-        elif previous == None:
-            temp.setNext(self.head)
-            if self.capacity == 1:
-                self.tail = self.head
-            self.head.setPrev(temp)
-            self.head = temp
-        elif current == None:
-            temp.setPrev(self.tail)
-            self.tail.setNext(temp)
-            self.tail = temp
-            self.tail.setNext(None)
-        else:
-            temp.setNext(current)
-            temp.setPrev(previous)
-            previous.setNext(temp)
-            current.setPrev(temp)
-        self.capacity+=1
-    def remove(self,item):
-    	index = 0
-    	start = self.head
-    	while index < self.capacity:
-    		if start.getData() == item:
-    			if self.capacity == 1:
-    				self.head = None
-    				self.tail = None
-    			elif index == 0: 
-    				self.head = start.getNext()
-    			elif index == self.capacity-1:
-    				start.getPrev().setNext(None)
-    				self.tail = start.getPrev()
-    			else:
-    				start.getNext().setPrev(start.getPrev())
-    				start.getPrev().setNext(start.getNext())
-    				
-    			
-    			self.capacity-=1
-    			break
-    		
-    		else:
-    			start = start.getNext()
-    			index+=1
+	def add(self,item):
+		'''Adds item to the list at the proper place so it remains sorted'''
+		'''Assume that the item is not already in the list'''
+		'''Takes an item and returns nothing'''
+		current = self.head
+		previous = None
+		stop = False
+		while current != None and not stop:
+			if current.getData() > item: 
+				stop = True
+			else:
+				previous = current
+				current = current.getNext()
+		temp = Node(item)
+		if self.capacity == 0:
+			temp.setNext(None)
+			temp.setPrev(None)
+			self.head = temp
+			self.tail = temp
+		elif previous == None:
+			temp.setNext(self.head)
+			if self.capacity == 1:
+				self.tail = self.head
+			self.head.setPrev(temp)
+			self.head = temp
+		elif current == None:
+			temp.setPrev(self.tail)
+			self.tail.setNext(temp)
+			self.tail = temp
+			self.tail.setNext(None)
+		else:
+			temp.setNext(current)
+			temp.setPrev(previous)
+			previous.setNext(temp)
+			current.setPrev(temp)
+		self.capacity+=1
+	def remove(self,item):
+		'''Removes item from the list'''
+		'''Assumes that the item is in the list'''
+		'''Doesn't return anything'''
+		index = 0
+		start = self.head
+		while index < self.capacity:
+			if start.getData() == item:
+				if self.capacity == 1:
+					self.head = None
+					self.tail = None
+				elif index == 0: 
+					self.head = start.getNext()
+				elif index == self.capacity-1:
+					start.getPrev().setNext(None)
+					self.tail = start.getPrev()
+				else:
+					start.getNext().setPrev(start.getPrev())
+					start.getPrev().setNext(start.getNext())
+				self.capacity-=1
+				break
+			else:
+				start = start.getNext()
+				index+=1    
+	def search_forward(self,item):
+		'''Searches for the item by starting at the head'''
+		'''Returns True if the item is in the list'''
+		index = 0
+		start = self.head
+		while index < self.capacity:
+			if start.getData() == item:
+				return True
+			else:
+				start = start.getNext()
+				index+=1
+		return False
+	def search_backward(self,item):
+		'''Searches for the item by starting at the tail'''
+		'''Returns True if the item is in the list'''
+		index = self.capacity -1 
+		start = self.tail
+		while index >= 0:
+			if start.getData() == item:
+				return True
+			else:
+				start = start.getPrev()
+				index-=1 
+		return False  
+	def is_empty(self):
+		'''Returns if the list is empty (size == 0)'''
+		return self.capacity == 0
+	
+	def size(self):
+		'''Returns the size of the list'''
+		return self.capacity
+	
+	def index(self,item):
+		'''Returns the index of the item'''
+		'''Assumes that the item is in the list'''
+		index = 0
+		start = self.head
+		while index < self.capacity:
+			if start.getData() == item:
+				return index
+			else:
+				start= start.getNext()
+				index += 1
+	def pop(self,pos=None):
+		'''Removes and returns the item at the given position'''
+		'''Position defaults to the last position is not given'''
+		'''Removes and returns the item at the given position by searching from front if pos <= size//2'''
+		'''And from rear if pos > size//2'''
+		if pos is None:
+			temp = self.tail.getData()
+			self.tail = self.tail.getPrev()
+			if self.capacity == 1:
+				return temp
+			self.tail.setNext(None)
+			self.capacity-=1
+			return temp
+		index = 0
+		start = self.head
+		if pos <= self.capacity//2: 
+			while index < self.capacity:
+				if index == pos:
+					temp = start.getData()
+					if self.capacity == 1:
+						self.head = None
+						self.tail = None
+					elif index == 0: 
+						self.head = start.getNext()
+					elif index == self.capacity-1:
+						start.getPrev().setNext(None)
+						self.tail = start.getPrev()
+					else:
+						start.getNext().setPrev(start.getPrev())
+						start.getPrev().setNext(start.getNext())
+					self.capacity-=1
+					return temp
+				
+				else:
+					start = start.getNext()
+					index+=1
+		elif pos > self.capacity//2:
+			index = self.capacity -1 
+			start = self.tail
+			while index >= 0:
+				if index == pos:
+					temp = start.getData()
+					if self.capacity == 1:
+						self.head = None
+						self.tail = None
+					elif index == 0: 
+						self.head = start.getNext()
+					elif index == self.capacity-1:
+						start.getPrev().setNext(None)
+						self.tail = start.getPrev()
+					else:
+						start.getPrev().setNext(start.getNext())
+						start.getNext().setPrev(start.getPrev())
+						
+					self.capacity-=1
+					return temp
+				else:
+					start = start.getPrev()
+					index-=1
 
-    
-    # def search_forward(item):
-    
-    # def search_backward(item):
-    
-    def is_empty(self):
-    	return self.capacity == 0
-    
-    def size(self):
-        return self.capacity
-    
-    def index(self,item):
-    	index = 0
-    	start = self.head
-    	while index < self.capacity:
-    		if start.getData() == item:
-    			return index
-    		else:
-    			start= start.getNext()
-    			index += 1
-
-
-    
-    def pop(self,pos=None):
-    	'''Removes and returns the item at the given position'''
-    	'''Position defaults to the last position is not given'''
-    	if pos is None:
-    		temp = self.tail.getData()
-    		self.tail = self.tail.getPrev()
-    		if self.capacity == 1:
-    			return temp
-    		self.tail.setNext(None)
-    		self.capacity-=1
-    		return temp
-    	index = 0
-    	start = self.head
-    	while index < self.capacity:
-    		if index == pos:
-    			temp = start.getData()
-    			if self.capacity == 1:
-    				self.head = None
-    				self.tail = None
-    			elif index == 0: 
-    				self.head = start.getNext()
-    			elif index == self.capacity-1:
-    				start.getPrev().setNext(None)
-    				self.tail = start.getPrev()
-    			else:
-    				start.getNext().setPrev(start.getPrev())
-    				start.getPrev().setNext(start.getNext())
-    				
-    			
-    			self.capacity-=1
-    			return temp
-    		
-    		else:
-    			start = start.getNext()
-    			index+=1
 class Node:
-    def __init__(self,initdata):
-        self.data=initdata
-        self.next=None
-        self.prev=None
-    def getData(self):
-        return self.data
-    def getNext(self):
-        return self.next
-    def getPrev(self):
-        return self.prev
-    def setData(self,newdata):
-        self.data= newdata
-    def setNext(self,newnext):
-        self.next = newnext
-    def setPrev(self,newprev):
-        self.prev = newprev
+	'''Node that contains references to the next and previous node for use in a doubly-linked list'''
+	def __init__(self,initdata):
+		self.data=initdata
+		self.next=None
+		self.prev=None
+	def getData(self):
+		return self.data
+	def getNext(self):
+		return self.next
+	def getPrev(self):
+		return self.prev
+	def setData(self,newdata):
+		self.data= newdata
+	def setNext(self,newnext):
+		self.next = newnext
+	def setPrev(self,newprev):
+		self.prev = newprev
--- Lab4/ordered_list_tests.py
+++ Lab4/ordered_list_tests.py
@@ -1,8 +1,9 @@
 import unittest
 from ordered_list import *
 class Test_OrderedList(unittest.TestCase):
-	''' Tests the add function '''
+	
 	def test_add0(self):
+		''' Tests the add, index, and remove function '''
 		g = OrderedList()
 		self.assertTrue(g.is_empty())
 		g.add(1)
@@ -40,8 +41,9 @@ class Test_OrderedList(unittest.TestCase):
 		g.remove(0)
 		g.remove(2)
 		self.assertTrue(g.is_empty())
-	''' Tests the add function with end cases (head and tail exceptions) '''
+	
 	def test_add1(self):
+		''' Tests the add function with end cases (head and tail exceptions) '''
 		h = OrderedList()
 		self.assertTrue(h.is_empty(),True)
 		h.add(1)
@@ -68,8 +70,31 @@ class Test_OrderedList(unittest.TestCase):
 		self.assertEqual(h.head.getNext().getNext().getNext().getNext().getData(),3)
 		self.assertEqual(h.head.getNext().getNext().getNext().getNext().getNext().getData(),4)
 		self.assertEqual(h.size(),6)
+	
+	def test_addremove_withsearches(self):
+		'''Tests add remove with searches'''
+		l = OrderedList()
+		self.assertTrue(l.is_empty())
+		l.add(1)
+		l.add(2)
+		self.assertFalse(l.is_empty())
+		l.add(3)
+		l.add(4)
+		self.assertFalse(l.is_empty())
+		self.assertEqual(l.index(3),2)
+		l.remove(1)
+		self.assertEqual(l.index(3),1)
+		self.assertFalse(l.is_empty())
+		self.assertTrue(l.search_forward(2))
+		l.remove(2)
+		self.assertFalse(l.search_forward(2))
+		self.assertTrue(l.search_backward(3))
+		l.remove(3)
+		self.assertFalse(l.search_backward(3))
+		l.remove(4)
+		self.assertTrue(l.is_empty())
 	def test_array_endpop(self):
-		"""Tests the add and pops without an argument"""
+		'''Tests the add and pops without an argument'''
 		l = OrderedList()
 		self.assertTrue(l.is_empty())
 		l.add(1)
@@ -86,7 +111,7 @@ class Test_OrderedList(unittest.TestCase):
 		self.assertEqual(l.pop(),2)
 		self.assertEqual(l.pop(),1)
 	def test_array_harder_pop(self):
-		"""Tests add and pops from front and end of the list"""
+		'''Tests add and pops from front and end of the list'''
 		l = OrderedList()
 		self.assertTrue(l.is_empty())
 		l.add(1)
@@ -100,7 +125,7 @@ class Test_OrderedList(unittest.TestCase):
 		self.assertEqual(l.pop(1),3)
 		self.assertEqual(l.pop(),2)
 	def test_array_hardest_pop(self):
-		"""Tests adding out of order and popping from front and end of the lsit"""
+		'''Tests adding out of order and popping from front and end of the list'''
 		l = OrderedList()
 		self.assertTrue(l.is_empty())
 		l.add(1)
