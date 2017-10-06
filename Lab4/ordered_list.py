865ba598bc99b0703d93b4497269d61770b40d1f
Tiger Sun <sunchester@Tigers-MacBook-Pro.local>
2017-10-06 13:57:13 -0700

    Added pop and remove functions. Added additional TestCases. Changed add function

--- Lab4/ordered_list.py
+++ Lab4/ordered_list.py
@@ -26,6 +26,7 @@ class OrderedList():
             temp.setNext(self.head)
             if self.capacity == 1:
                 self.tail = self.head
+            self.head.setPrev(temp)
             self.head = temp
         elif current == None:
             temp.setPrev(self.tail)
@@ -38,7 +39,31 @@ class OrderedList():
             previous.setNext(temp)
             current.setPrev(temp)
         self.capacity+=1
-    # def remove(item):
+    def remove(self,item):
+    	index = 0
+    	start = self.head
+    	while index < self.capacity:
+    		if start.getData() == item:
+    			if self.capacity == 1:
+    				self.head = None
+    				self.tail = None
+    			elif index == 0: 
+    				self.head = start.getNext()
+    			elif index == self.capacity-1:
+    				start.getPrev().setNext(None)
+    				self.tail = start.getPrev()
+    			else:
+    				start.getNext().setPrev(start.getPrev())
+    				start.getPrev().setNext(start.getNext())
+    				
+    			
+    			self.capacity-=1
+    			break
+    		
+    		else:
+    			start = start.getNext()
+    			index+=1
+
     
     # def search_forward(item):
     
@@ -62,10 +87,41 @@ class OrderedList():
 
 
     
-    # def pop():
-    
-    # def pop(pos):
-
+    def pop(self,pos=None):
+    	'''Removes and returns the item at the given position'''
+    	'''Position defaults to the last position is not given'''
+    	if pos is None:
+    		temp = self.tail.getData()
+    		self.tail = self.tail.getPrev()
+    		if self.capacity == 1:
+    			return temp
+    		self.tail.setNext(None)
+    		self.capacity-=1
+    		return temp
+    	index = 0
+    	start = self.head
+    	while index < self.capacity:
+    		if index == pos:
+    			temp = start.getData()
+    			if self.capacity == 1:
+    				self.head = None
+    				self.tail = None
+    			elif index == 0: 
+    				self.head = start.getNext()
+    			elif index == self.capacity-1:
+    				start.getPrev().setNext(None)
+    				self.tail = start.getPrev()
+    			else:
+    				start.getNext().setPrev(start.getPrev())
+    				start.getPrev().setNext(start.getNext())
+    				
+    			
+    			self.capacity-=1
+    			return temp
+    		
+    		else:
+    			start = start.getNext()
+    			index+=1
 class Node:
     def __init__(self,initdata):
         self.data=initdata
--- Lab4/ordered_list_tests.py
+++ Lab4/ordered_list_tests.py
@@ -4,7 +4,13 @@ class Test_OrderedList(unittest.TestCase):
 	''' Tests the add function '''
 	def test_add0(self):
 		g = OrderedList()
-		self.assertTrue(g.is_empty(),True)
+		self.assertTrue(g.is_empty())
+		g.add(1)
+		self.assertEqual(g.index(1),0)
+		g.remove(1)
+		self.assertEqual(g.head, None)
+		self.assertEqual(g.tail, None)
+		self.assertTrue(g.is_empty())
 		g.add(1)
 		self.assertEqual(g.index(1),0)
 		self.assertFalse(g.is_empty(),False)
@@ -22,6 +28,18 @@ class Test_OrderedList(unittest.TestCase):
 		self.assertEqual(g.head.getData(),0)
 		self.assertEqual(g.tail.getData(),2)
 		self.assertEqual(g.size(),3)
+		g.remove(0)
+		self.assertEqual(g.head.getData(),1)
+		g.add(0)
+		g.remove(2)
+		self.assertEqual(g.tail.getData(),1)
+		g.add(2)
+		self.assertFalse(g.is_empty())
+		g.remove(1)
+		self.assertEqual(g.index(2),1)
+		g.remove(0)
+		g.remove(2)
+		self.assertTrue(g.is_empty())
 	''' Tests the add function with end cases (head and tail exceptions) '''
 	def test_add1(self):
 		h = OrderedList()
@@ -50,6 +68,58 @@ class Test_OrderedList(unittest.TestCase):
 		self.assertEqual(h.head.getNext().getNext().getNext().getNext().getData(),3)
 		self.assertEqual(h.head.getNext().getNext().getNext().getNext().getNext().getData(),4)
 		self.assertEqual(h.size(),6)
+	def test_array_endpop(self):
+		"""Tests the add and pops without an argument"""
+		l = OrderedList()
+		self.assertTrue(l.is_empty())
+		l.add(1)
+		l.add(2)
+		self.assertFalse(l.is_empty())
+		l.add(3)
+		l.add(4)
+		self.assertFalse(l.is_empty())
+		self.assertEqual(l.index(3),2)
+		
+		self.assertEqual(l.pop(),4)
+		self.assertEqual(l.pop(),3)
+
+		self.assertEqual(l.pop(),2)
+		self.assertEqual(l.pop(),1)
+	def test_array_harder_pop(self):
+		"""Tests add and pops from front and end of the list"""
+		l = OrderedList()
+		self.assertTrue(l.is_empty())
+		l.add(1)
+		l.add(2)
+		self.assertFalse(l.is_empty())
+		l.add(3)
+		l.add(4)
+		
+		self.assertEqual(l.pop(0),1)
+		self.assertEqual(l.pop(2),4)
+		self.assertEqual(l.pop(1),3)
+		self.assertEqual(l.pop(),2)
+	def test_array_hardest_pop(self):
+		"""Tests adding out of order and popping from front and end of the lsit"""
+		l = OrderedList()
+		self.assertTrue(l.is_empty())
+		l.add(1)
+		l.add(5)
+		self.assertFalse(l.is_empty())
+		l.add(4)
+		l.add(3)
+		l.add(2)
+		
+		self.assertEqual(l.pop(),5)
+		self.assertEqual(l.pop(1),2)
+		self.assertEqual(l.pop(),4)
+		self.assertEqual(l.pop(0),1)
+		self.assertEqual(l.pop(),3)
+
+
+
+
+
 if __name__ == "__main__":
 	unittest.main()
 
