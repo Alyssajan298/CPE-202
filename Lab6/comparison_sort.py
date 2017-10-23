0417bfc83e70bd8c708f1a01c3bae538874c5ee8
Tiger Sun <sunchester@Tigers-MacBook-Pro.local>
2017-10-22 22:36:00 -0700

    Added Lab6

---
+++ Lab6/comparison_sort.py
@@ -0,0 +1,9 @@
+def insert_sort(alist):
+    """Sorts a List by Insertion"""
+    None
+def select_sort(alist):
+    """Sorts a List by Selection"""
+    None
+def merge_sort(alist):
+    """Sorts a List by Merging"""
+    None
