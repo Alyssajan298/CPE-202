70c5b542efd171a418b6b498fcbfcbfb09b9eee8
Tiger Sun <suntiger.stdt@gmail.com>
2017-10-24 15:12:41 -0700

    Testing Git Functionality

--- Assignment3/huffman.py
+++ Assignment3/huffman.py
@@ -1,35 +1,40 @@
+a = 'a'
+print float(a)
 
-class HuffmanNode:
-	def __init__(self, char, freq):
-		self.char = char  # actually the character code
-		self.freq = freq
-		self.code = None
-		self.left = None
-		self.right = None
 
-	def set_code (self, code):
-		self.code = code
 
-	def set_left (self, node):
-		self.left = node
-
-	def set_right (self, node):
-		self.right = node
-
-def comes_before (a, b) :
-	"""returns true if tree rooted at node a comes before tree rooted at node b """
-
-def combine (a, b) :
-	""" creates a new huffman node with children a and b with combined freq with name of the left child """
-
-def cnt_freq(filename):
-
-def create_huff_tree(char_freq):
-
-def create_code (node):
-
-def tree_preord (node):
-
-def huffman_encode(in_file, out_file):
-
-def huffman_decode(freqs, encoded_file, decode_file):
+#
+# class HuffmanNode:
+# 	def __init__(self, char, freq):
+# 		self.char = char  # actually the character code
+# 		self.freq = freq
+# 		self.code = None
+# 		self.left = Nonex
+# 		self.right = None
+#
+# 	def set_code (self, code):
+# 		self.code = code
+#
+# 	def set_left (self, node):
+# 		self.left = node
+#
+# 	def set_right (self, node):
+# 		self.right = node
+#
+# def comes_before (a, b) :
+# 	"""returns true if tree rooted at node a comes before tree rooted at node b """
+#
+# def combine (a, b) :
+# 	""" creates a new huffman node with children a and b with combined freq with name of the left child """
+#
+# def cnt_freq(filename):
+#
+# def create_huff_tree(char_freq):
+#
+# def create_code (node):
+#
+# def tree_preord (node):
+#
+# def huffman_encode(in_file, out_file):
+#
+# def huffman_decode(freqs, encoded_file, decode_file):
