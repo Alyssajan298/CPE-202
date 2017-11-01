b71a1a678a0a1dc17182e30e2941e026f1252c52
Tiger Sun <suntiger.stdt@gmail.com>
2017-10-31 17:59:34 -0700

    Added new Functions to Lab7

---
+++ Lab7/heap_file_tests.py
@@ -0,0 +1,16 @@
+"""
+Module Docstring
+"""
+import unittest
+from heap_lab import *
+
+
+class TestHeap(unittest.TestCase):
+    """
+    Docstring
+    """
+    def test_nsert(self):
+        """
+        Docstring
+        """
+        pass
--- Lab7/heap_lab.py
+++ Lab7/heap_lab.py
@@ -1,68 +1,95 @@
 """
 Implementation of a MaxHeap
 """
-class MaxHeap():
 
+
+class MaxHeap(object):
+    """
+    MaxHeap Class Bottom-Up
+    """
     def __init__(self, capacity=50):
         """
         Initialize class MaxHeap
         """
-        self.size = capacity
+        self.size = 0
+        self.heaplist = [capacity]
+
     def insert(self, item):
         """
-        docstring
+        Insert an element. Returns True/False
+        if the insert is successful.
         """
-        pass
+        if self.size == len(self.heaplist):
+            return False
+        self.heaplist.append(item)
+        self.size += 1
+        self.perc_up(self.size)
+        return True
+
     def find_max(self):
         """
         docstring
         """
         pass
+
     def del_max(self):
         """
         docstring
         """
         pass
+
     def heap_contents(self):
         """
         docstring
         """
         pass
+
     def build_heap(self, alist):
         """
         docstring
         """
         pass
+
     def is_empty(self):
         """
-        docstring
+        Returns True/False if
+        Heap is empty.
         """
-        pass
+        return self.size == 0
+
     def is_full(self):
         """
-        docstring
+        Returns True/False if
+        Heap is full.
         """
-        pass
+        return self.size == len(self.heaplist)
+
     def get_heap_cap(self):
         """
         docstring
         """
         pass
+
     def get_heap_size(self):
         """
         docstring
         """
         pass
-    def perc_down(self):
+
+    def perc_down(self, i):
         """
-        docstring
-        """
-        pass
-    def perc_up(self):
-        """
-        docstring
+        Function that moves
+        elements in Heap down.
         """
         pass
+
+    def perc_up(self, i):
+        """
+        Function that moves
+        elements in Heap up.
+        """
+        pass
+
     def heap_sort_increase(self, alist):
         """
         docstring
