45bf06881d1e808d9b4764f6080bd727fba49759
Tiger Sun <suntiger.stdt@gmail.com>
2017-11-07 09:45:40 -0800

    Added Lab8

---
+++ Lab8/sep_chain_ht.py
@@ -0,0 +1,50 @@
+"""
+Lab8 Separate Chaining Hash Table
+"""
+
+
+class MyHashTable(object):
+    """
+    Class to Create a Separate Chaining Hash Table
+    """
+    def __init__(self, table_size=11):
+        """
+        Initialize the Hash Table
+        """
+        pass
+
+    def insert(self, key, item):
+        """
+        Insert Function that takes a key and item
+        """
+        pass
+
+    def get(self, key):
+        """
+        Uses given key to find and return the item, key pair
+        """
+        pass
+
+    def remove(self, key):
+        """
+        Removes item, key pair and returns it
+        """
+        pass
+
+    def size(self):
+        """
+        Returns number of pairs in the Hash Table
+        """
+        pass
+
+    def load_factor(self):
+        """
+        Returns current load factor
+        """
+        pass
+
+    def collisions(self):
+        """
+        Returns number of collisions during insertions
+        """
+        pass
