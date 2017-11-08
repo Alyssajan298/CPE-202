b92d1a9c99c2d9b98129948f766358f01b4995f1
Tiger Sun <suntiger.stdt@gmail.com>
2017-11-07 18:51:59 -0800

    Added get Function

--- Lab8/sep_chain_ht.py
+++ Lab8/sep_chain_ht.py
@@ -12,21 +12,34 @@ class MyHashTable(object):
         Initialize the Hash Table
         """
         self.tsize = table_size
+        self.count = 0
         self.hashlist = []
         for i in range(table_size):
-            self.hashlist.append(None)
+            self.hashlist.append([])
 
     def insert(self, key, item):
         """
         Insert Function that takes a key and item
         """
-        pass
+        if self.count / self.tsize == 1.5:
+            self.rehash()
+        index = key % self.tsize
+        self.hashlist[index].append((key, item))
+        self.count += 1
 
     def get(self, key):
         """
         Uses given key to find and return the item, key pair
         """
-        pass
+        index = key % self.tsize
+        i = 0
+        hashsize = len(self.hashlist[index])
+        searchlist = self.hashlist[index]
+        while i < hashsize:
+            if (searchlist[i])[0] == key:
+                return searchlist[i]
+            i += 1
+        raise LookupError
 
     def remove(self, key):
         """
@@ -51,6 +64,3 @@ class MyHashTable(object):
         Returns number of collisions during insertions
         """
         pass
-hashbrown = MyHashTable()
-print(hashbrown.tsize)
-print(hashbrown.hashlist)
