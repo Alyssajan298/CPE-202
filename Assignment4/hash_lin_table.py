48200e69845ff316049c1c94c82080d4584835ea
Tiger Sun <suntiger.stdt@gmail.com>
2017-11-14 12:02:25 -0800

    Added Skeletons of Assignment4

--- Assignment4/hash_lin_table.py
+++ Assignment4/hash_lin_table.py
@@ -13,3 +13,39 @@ class HashTableLinPr:
         self.tsize = size
         self.count = 0
         self.hashlist = []
+
+    def read_stop(self, filename):
+        """
+        Read Stop Words and Hashes into Table
+        """
+        pass
+
+    def read_file(self, filename, stoptable):
+        """
+        Read File, Hashes into Table, and Filters Stop Words
+        """
+        pass
+
+    def get_tablesize(self):
+        """
+        Returns Size of Hash Table
+        """
+        pass
+
+    def save_concordance(self, outputfilename):
+        """
+        Saves Concordance
+        """
+        pass
+
+    def get_load_fact(self):
+        """
+        Returns Load Factor
+        """
+        pass
+
+    def myhash(self, key, table_size):
+        """
+        Returns Hash Value
+        """
+        pass
--- Assignment4/hash_quad_table.py
+++ Assignment4/hash_quad_table.py
@@ -2,7 +2,50 @@
 Implementation of Quadratic Probing
 Hash Table
 """
-class HashTableQuadPr:
+class HashTableLinPr:
     """
     Quadratic Hash Class
     """
+    def __init__(self, size):
+        """
+        Initialization of Hash Table
+        """
+        self.tsize = size
+        self.count = 0
+        self.hashlist = []
+
+    def read_stop(self, filename):
+        """
+        Read Stop Words and Hashes into Table
+        """
+        pass
+
+    def read_file(self, filename, stoptable):
+        """
+        Read File, Hashes into Table, and Filters Stop Words
+        """
+        pass
+
+    def get_tablesize(self):
+        """
+        Returns Size of Hash Table
+        """
+        pass
+
+    def save_concordance(self, outputfilename):
+        """
+        Saves Concordance
+        """
+        pass
+
+    def get_load_fact(self):
+        """
+        Returns Load Factor
+        """
+        pass
+
+    def myhash(self, key, table_size):
+        """
+        Returns Hash Value
+        """
+        pass
