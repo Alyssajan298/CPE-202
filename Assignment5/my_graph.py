28f2fc6e7a6827b6f0e524b88e4a4f23022b76dc
Tiger Sun <suntiger.stdt@gmail.com>
2017-11-23 13:32:41 -0800

    Added Assignment5 and Skeleton

---
+++ Assignment5/my_graph.py
@@ -0,0 +1,26 @@
+"""
+Assignment for Graphs
+"""
+
+
+class MyGraph(object):
+    """
+    MyGraph Class Object
+    """
+    def __init__(self, filename):
+        """
+        Initialization of MyGraph
+        """
+        pass
+
+    def conn_components(self):
+        """
+        Returns List of Lists
+        """
+        pass
+
+    def bicolor(self):
+        """
+        Returns Boolean If Bicolorable
+        """
+        pass
